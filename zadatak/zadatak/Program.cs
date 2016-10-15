de8366c6afcf2fd42f64d07a904d37674eff648f
Renato Orgulan <renato.orgulan@fer.hr>
2016-10-15 14:05:50 +0200

    edited project

---
+++ zadatak/zadatak/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntegerList.ListExample(new IntegerList(8));
+        }
+    }
+}
