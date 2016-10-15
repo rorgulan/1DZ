cf2f0c13cd87f0e585ba17520fb8e19ae8399191
Renato Orgulan <renato.orgulan@fer.hr>
2016-10-15 13:10:40 +0200

    changed project

---
+++ Zadatak1/Zadatak1/Program1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak1
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+             IntegerList.ListExample(new IntegerList(8));
+        }
+    }
+}
