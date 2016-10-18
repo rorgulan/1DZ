56632f37f48e9e796aaa9ea3cab36be60b71abc7
Renato Orgulan <renato.orgulan@fer.hr>
2016-10-18 22:26:06 +0200

    prvi zadatak gotov

--- zadatak/Pong/Game1.cs
+++ zadatak/Pong/Game1.cs
@@ -12,6 +12,67 @@ namespace Pong
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        /// <summary >
+        /// Game background representation
+        /// </ summary >
+        public class Background : Sprite
+        {
+            public Background(int width, int height) : base(width, height)
+            {
+            }
+        }
+        /// <summary >
+        /// Game ball object representation
+        /// </ summary >
+        public class Ball : Sprite
+        {
+            /// <summary >
+            /// Defines current ball speed in time .
+            /// </ summary >
+            public float Speed { get; set; }
+            public float BumpSpeedIncreaseFactor { get; set; }
+            /// <summary >
+            /// Defines ball direction .
+            /// Valid values ( -1 , -1) , (1 ,1) , (1 , -1) , ( -1 ,1).
+            /// Using Vector2 to simplify game calculation . Potentially
+            /// dangerous because vector 2 can swallow other values as well .
+            /// OPTIONAL TODO : create your own , more suitable type
+            /// </ summary >
+            public Vector2 Direction { get; set; }
+            public Ball(int size, float speed, float
+            defaultBallBumpSpeedIncreaseFactor) : base(size, size)
+            {
+                Speed = speed;
+                BumpSpeedIncreaseFactor = defaultBallBumpSpeedIncreaseFactor;
+                // Initial direction
+                Direction = new Vector2(1, 1);
+            }
+        }
+        /// <summary >
+        /// Represents player paddle .
+        /// </ summary >
+        public class Paddle : Sprite
+        {
+            /// <summary >
+            /// Current paddle speed in time
+            /// </ summary >
+            public float Speed { get; set; }
+            public Paddle(int width, int height, float initialSpeed) : base(width,
+            height)
+            {
+                Speed = initialSpeed;
+            }
+            /// <summary >
+            /// Overriding draw method . Masking paddle texture with black color .
+            /// </ summary >
+            public override void DrawSpriteOnScreen(SpriteBatch spriteBatch)
+            {
+                spriteBatch.Draw(Texture, new Vector2(X, Y), new Rectangle(0, 0,
+                Width, Height), Color.GhostWhite);
+            }
+        }
+
+
         public abstract class Sprite
         {
             public float X { get; set; }
@@ -23,7 +84,9 @@ namespace Pong
             /// Texture2D is a type defined in Monogame framework .
             /// </ summary >
 
-            public Texture2D Texture { get; set; }            protected Sprite(int width, int height, float x = 0, float y = 0)
+            public Texture2D Texture { get; set; }
+
+            protected Sprite(int width, int height, float x = 0, float y = 0)
             {
                 X = x;
                 Y = y;
@@ -37,7 +100,9 @@ namespace Pong
             {
                 spriteBatch.Draw(Texture, new Vector2(X, Y), new Rectangle(0, 0,
                 Width, Height), Color.White);
-            }
+            }
+
+
 
         }
 
